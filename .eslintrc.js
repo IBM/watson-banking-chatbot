eb58f112f61f28b845eef19392aa4e7984b70023
Mark Sturdevant <mark.sturdevant@ibm.com>
2017-06-07 15:45:09 -0700

    Move Watson Disco and Conv setup to lib for testability and reuse. Add tests.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   "env": {
     "node": true,
+    "mocha": true,
   },
   "plugins": [
     "node",
@@ -15,6 +16,7 @@ module.exports = {
   "rules": {
     "no-console": 0,
     "no-process-exit": 0,
+    "node/no-unpublished-require": ["error", {"allowModules": ["chai", "sinon", "sinon-test"]}],
     "prettier/prettier": ["error", {"singleQuote": true, "printWidth": 160}],
     "prefer-const": "error",
     "prefer-rest-params": "off",
